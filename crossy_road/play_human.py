6effb330bfff3185db1d4fd9e984f561aaa52079
Folderas <marcinwolder7@gmail.com>
2026-04-28 23:03:11 +0200

    Human playable game

--- crossy_road/play_human.py
+++ crossy_road/play_human.py
@@ -6,22 +6,25 @@ from crossy_road import CrossyRoadEnv
 def main() -> None:
     env = CrossyRoadEnv(render_mode="human")
     obs, info = env.reset(seed=42)
-    done = False
+    episode = 1
 
     try:
-        while not done:
+        while True:
             action = env.get_human_action()
             obs, reward, terminated, truncated, info = env.step(action)
-            done = terminated or truncated
+            if terminated or truncated:
+                print(
+                    {
+                        "episode": episode,
+                        "finished": info.get("finished", False),
+                        "collision": info.get("collision", False),
+                        "steps": info.get("steps"),
+                        "progress": round(info.get("progress", 0.0), 3),
+                    }
+                )
+                episode += 1
+                obs, info = env.reset()
 
-        print(
-            {
-                "finished": info.get("finished", False),
-                "collision": info.get("collision", False),
-                "steps": info.get("steps"),
-                "progress": round(info.get("progress", 0.0), 3),
-            }
-        )
     except KeyboardInterrupt:
         print("Stopped by user.")
     finally:
